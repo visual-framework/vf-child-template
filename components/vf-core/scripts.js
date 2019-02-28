0dc783072502a925f07d31fec27662f56d8cc34b
Ken Hawkins <khawkins98@gmail.com>
2019-02-28 13:07:21 +0100

    Remove unused JS references

--- components/vf-core/scripts.js
+++ components/vf-core/scripts.js
@@ -7,25 +7,25 @@
  */
 
 // All VF JS
-import { vfBanner } from 'vf-banner/vf-banner';
-vfBanner();
+// import { vfBanner } from 'vf-banner/vf-banner';
+// vfBanner();
 
 import { vfTabs } from 'vf-tabs/vf-tabs';
 vfTabs();
 
-import 'vf-form/vf-form__core/assets/float-labels.js';
+// import 'vf-form/vf-form__core/assets/float-labels.js';
 
 // All EMBL JS
-import { emblConditionalEdit } from 'embl-conditional-edit/embl-conditional-edit';
-emblConditionalEdit();
+// import { emblConditionalEdit } from 'embl-conditional-edit/embl-conditional-edit';
+// emblConditionalEdit();
+//
+// import { emblContentHubLoaderHtmlImports } from 'embl-content-hub-loader/embl-content-hub-loader__html-imports';
+// import { emblContentHubFetch } from 'embl-content-hub-loader/embl-content-hub-loader__fetch';
+// import { emblContentHub } from 'embl-content-hub-loader/embl-content-hub-loader';
+// emblContentHub();
 
-import { emblContentHubLoaderHtmlImports } from 'embl-content-hub-loader/embl-content-hub-loader__html-imports';
-import { emblContentHubFetch } from 'embl-content-hub-loader/embl-content-hub-loader__fetch';
-import { emblContentHub } from 'embl-content-hub-loader/embl-content-hub-loader';
-emblContentHub();
-
-import { emblBreadcrumbs } from 'embl-breadcrumbs-lookup/embl-breadcrumbs-lookup';
-emblBreadcrumbs();
-
-import { emblContentMetaProperties_Read } from 'embl-content-meta-properties/embl-content-meta-properties';
+// import { emblBreadcrumbs } from 'embl-breadcrumbs-lookup/embl-breadcrumbs-lookup';
+// emblBreadcrumbs();
+//
+// import { emblContentMetaProperties_Read } from 'embl-content-meta-properties/embl-content-meta-properties';
 // No default invokation
